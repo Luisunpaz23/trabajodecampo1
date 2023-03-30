af934c921b3bd24a033bc4258139e443af3c19a3
Luisunpaz23 <luisuniversidadunpaz@gmail.com>
2023-03-30 11:13:05 -0300

    parte 2 clase 3

--- src/app.js
+++ src/app.js
@@ -1,7 +1,26 @@
-console.log('Hola mundo');
-const express = require('express');
+const express = require("express");
+
 const app = express();
-app.get ('/api', (req,res)=>{console.log('Paso por api');
-res.json({mensaje:'HOLA MUNDO', estado: 'ok'}).status(200)
+
+const datos = require("../data/data.json");
+
+console.log(datos);
+
+app.get("/api", (req, res) => {
+    console.log("Paso por api");
+    res.json(
+        datos.personas
+    ).status(200);
 });
-app.listen(3000,()=>{console.log('escuchando en el puerto 3000')});
+
+
+
+getConnection = async () => {
+    try {
+        app.listen(3000, console.log("SERVER STARTED"));
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+getConnection();
